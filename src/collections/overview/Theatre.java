76e63621c4dac647ea6654a7483cfbff52ead1d1
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-02 13:24:45 -0400

    Accidentally placed static methods in the wrong place

--- src/collections/overview/Theatre.java
+++ src/collections/overview/Theatre.java
@@ -9,6 +9,23 @@ public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>();
 
+    static final Comparator<Seat> PRICE_ORDER;
+
+    static {
+        PRICE_ORDER = new Comparator<Seat>() {
+            @Override
+            public int compare(Seat seat1, Seat seat2) {
+                if (seat1.getPrice() < seat2.getPrice()) {
+                    return -1;
+                } else if (seat1.getPrice() > seat2.getPrice()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+    }
+
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
 
@@ -65,23 +82,6 @@ public class Theatre {
         @Override
         public int compareTo(Seat o) {
             return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
-        }
-
-        static final Comparator<Seat> PRICE_ORDER;
-
-        static {
-            PRICE_ORDER = new Comparator<Seat>() {
-                @Override
-                public int compare(Seat seat1, Seat seat2) {
-                    if (seat1.getPrice() < seat2.getPrice()) {
-                        return -1;
-                    } else if (seat1.getPrice() > seat2.getPrice()) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-            };
         }
 
         public boolean reserve() {
